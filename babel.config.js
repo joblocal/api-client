69bdca85d579d5c8d04d482d5da981a1433c9aae
Johannes Hofmann <johannes_hofmann@me.com>
2019-09-30 09:38:16 +0200

    [DR-13102-promise-based-token-support] enables babel to transpile async/await

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,16 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [['@babel/preset-env']];
+  const presets = [
+    ['@babel/preset-env', { useBuiltIns: false }],
+  ];
+
+  const plugins = [
+    '@babel/plugin-transform-runtime',
+  ];
 
   return {
     presets,
+    plugins,
   };
 };
